cc1e3e7754466e16f7a41045b87ccf59e434f95f
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2020-07-25 03:12:41 +0200

    the documentation for the fabric_drawing function is done

---
+++ R/fabric_drawing.R
@@ -0,0 +1,106 @@
+
+#' Create a canvas element for drawing
+#'
+#' @param cid the id of the canvas element
+#' @param cwidth the width of the canvas element
+#' @param cheight the height of the canvas element
+#' @param cfill the color of the canvas element. Default to #FFFFFF (white)
+#' @param drawingWidth the width of the drawing output. Default to 2
+#' @param drawingColor the color of the drawing output. Default to #1E1E1E (black)
+#'
+#' @return an HTML canvas element
+#' @export
+#'
+#' @examples
+#'if (interactive()) {
+#'
+#'ui <- fluidPage(
+#'
+#'    use_fabric(),
+#'
+#'    h1("Draw some stuff here"),
+#'
+#'    fabric_drawing(cid = "canvas1")
+#'
+#'    )
+#'
+#'server <- function(input, output) {}
+#'
+#'shinyApp(ui = ui, server = server)
+#'
+#'}
+
+
+
+
+fabric_drawing <- function(cid,
+                          cwidth = 800,
+                          cheight = 600,
+                          cfill = "#FFFFFF",
+                          drawingWidth = 2,
+                          drawingColor = "#1E1E1E"){
+
+
+
+htmltools::tagList(
+
+  htmltools::tags$canvas(id = cid, width = cwidth, height = cheight),
+
+  htmltools::tags$script(htmltools::HTML(glue::glue(
+    "
+
+var {cid} = new fabric.Canvas('{cid}', {{
+
+    isDrawingMode: true
+
+    }});
+
+{cid}.backgroundColor = '{cfill}';
+
+{cid}.freeDrawingBrush.width = {drawingWidth};
+
+{cid}.freeDrawingBrush.color = '{drawingColor}';
+
+
+  "
+  )))
+
+
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
