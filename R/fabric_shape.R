3f8357ba2665d2025ac4a6f07aaed29156bd19e3
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2020-07-25 04:09:20 +0200

    fabric_shape() documentation DONE

--- R/fabric_shape.R
+++ R/fabric_shape.R
@@ -1,4 +1,56 @@
 
+#' Create shapes inside a canvas
+#'
+#' @param cid the id of the canvas element
+#' @param cwidth the width of the canvas element. Defaults to 800
+#' @param cheight the height of the canvas element. Defaults to 600
+#' @param cfill the color of the canvas element
+#' @param shapeId the id of the shape object
+#' @param shape the shape of the object. Choices include 'Circle', 'Triangle' and 'Rect'. Defaults to 'Rect'
+#' @param left The shape's position from the left relative to the canvas element. Defaults to 100
+#' @param top The shape's position from the top relative to the canvas element. Defaults to 100
+#' @param fill the color of the shape. Defaults to 'red'
+#' @param width the width of the shape. Defaults to 200
+#' @param height the height of the shape. Defaults to 200
+#' @param angle the angle of rotation of the shape. Defaults to 0 (no rotation)
+#' @param opacity the opacity of the shape. Defaults to 1
+#' @param strokecolor the stroke color of the shape. Defaults to 'darkblue'
+#' @param strokewidth the stroke width of the shape. Defaults to 5.
+#' @param selectable logical. If TRUE, the user can modify interactively the shape. Defaults to TRUE
+#' @param isDrawingMode logical. If TRUE, the user can draw inside the canvas element.
+#' @param radius Mandatory if the chosen shape is a 'Circle'. Defaults to NULL
+#'
+#' @return a shape inside a canvas
+#' @export
+#'
+#' @examples
+#'
+#'if(interactive()){
+#'
+#'ui <- fluidPage(
+#'
+#'use_fabric(),
+#'
+#'h2("Below you'll find a red Rectangle with a darkblue stroke"),
+#'
+#'fabric_shape(cid = "canvas", shapeId = "shape1", shape = "Rect")
+#'
+#')
+#'
+#'server <- function(input, output) {
+#'
+#'
+#'}
+#'
+#'
+#'shinyApp(ui = ui, server = server)
+#'
+#'}
+#'
+
+
+
+
 
 fabric_shape <- function(cid,
                          cwidth = 800,
@@ -17,23 +69,41 @@ fabric_shape <- function(cid,
                          strokewidth = 5,
                          selectable = TRUE,
                          isDrawingMode = FALSE,
-                         radius = NULL){
+                         radius = NULL) {
 
 
-radius <- ifelse(!is.null(radius), glue::glue("radius:{radius}"), "")
+  if (!theme %in% c("Rect",
+                    "Circle",
+                    "Triangle", )) {
+    stop(paste0(theme, " theme is not available, did you misspelled it ?"))
+  }
 
 
-selectable <- ifelse(selectable == TRUE, "true", "false")
 
-isDrawingMode <- ifelse(isDrawingMode == TRUE, "true", "false")
+  if (shape == "Circle" &
+      is.null(radius)) {
+    stop("If you draw a Circle, you need to provide a radius")
+  }
+
+  radius <-
+    ifelse(!is.null(radius), glue::glue("radius:{radius}"), "")
 
 
-htmltools::tagList(
+  selectable <- ifelse(selectable == TRUE, "true", "false")
 
-  htmltools::tags$canvas(id = cid, width = cwidth, height = cheight),
+  isDrawingMode <- ifelse(isDrawingMode == TRUE, "true", "false")
 
-  htmltools::tags$script(htmltools::HTML(glue::glue(
-    "
+
+  htmltools::tagList(
+    htmltools::tags$canvas(
+      id = cid,
+      width = cwidth,
+      height = cheight
+    ),
+
+    htmltools::tags$script(htmltools::HTML(
+      glue::glue(
+        "
 
 var {cid} = new fabric.Canvas('{cid}', {{
 
@@ -65,11 +135,12 @@ selectable: {selectable},
 
 
   "
-  )))
+      )
+    ))
 
 
 
-)
+  )
 
 
 
